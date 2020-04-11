7db6c04b70cf69d3add7865ac7805d49e4837850
afmoyar <afmoyar@unal.edu.co>
2020-04-10 21:02:23 -0500

    erased former postgres instance of database

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,54 +1,6 @@
 const express = require ('express');
 const router = express.Router();
 
-//Setup Database connection
-const initOptions = {
-    schema: 'public'
-};
-
-const pgp = require('pg-promise')(initOptions);
-const connection = {
-    host: 'localhost', // server name or IP address;
-    port: 5432,
-    database: 'UN-Acarreo',
-    user: 'postgres',
-    password: 'admin',
-};
-
-const db = pgp(connection); // database instance;
-//end of the setup of database connection
-
-async function getInfo(){
-  //Select and return information
-  var info = await db.any('SELECT * FROM "User"');
-  return info;
-}
-
-async function insertUser(user){
-  //insert user
-  var insertion = await db.none('INSERT INTO "User"("User_name", "User_last_name", "User_password","User_addresss","User_E-mail")' +
-                                ' VALUES(${name}, ${lastname}, ${password},${address},${email})', user);
-  return;
-
-}
-
-//test insertion
-router.get('/insertuser', async function(req, res){
-  //The variable user can be changed in order to test user insertion with different data
-  var user = {name: 'Esteban', lastname: 'Jaramillo', password: '123456', address: 'cll fake 123', email: 'v@v.com'}
-  await insertUser(user);
-  res.json({Respuesta: 'Usuario insertado'})
-});
-
-//test get info
-router.get('/getinfo', async function(req, res){
-  var info = await getInfo()
-  console.log(info)
-  res.json(info)
-});
-
-
-
 //Route will be used to handle login POST requests
 router.post('/login', function(req, res){
   //TODO login user
@@ -56,23 +8,24 @@ router.post('/login', function(req, res){
 
 })
 
-
 //Route will be used to handle driver sign up POST requests
 router.post('/driver-signup', function(req, res){
   
   res.json({Api: 'Online'})
 });
 
-
+const UserController =require('../Controllers/UserController')
 //Route will be used to handle client sign up POST requests
-router.post('/client-signup', async function(req, res){
-  //TODO sign up client
-  let user = req.body.request;
-  await insertUser(user);
-  console.log(req.body.request); //Here should come the user data
-  res.json({Api: 'Online'})
-});
-
+router.post('/client-signup',async function(req,res){
+  let success = await UserController.createUser(req);
+  if(success==1)
+  {
+    console.log("added succesfully");
+  }
+  else if(succes==0){
+    console.log("error");
+  }
+} );
 
 //Route will be used to handle POST requests of service creation
 router.post('/create-service', function(req, res){
