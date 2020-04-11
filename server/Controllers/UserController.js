77180bb4b756b72913626791f43b7cfb1ad25998
afmoyar <afmoyar@unal.edu.co>
2020-04-10 21:01:13 -0500

    modified createUser to handle errors

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,18 +1,26 @@
 const UserModel = require('../Models/User');
-async function createUser(req,res)
+async function createUser(req)
 {
-    const{name, lastname, password, address, email}=req.body.request;
-    let newUser = await UserModel.create(
-        {
-            User_name: name,
-            User_last_name: lastname,
-            User_password: password,
-            User_address: address,
-            User_Email: email
-        }
-        ,{
-        fields: ['User_name','User_last_name','User_password','User_address','User_Email']
-        }
-    );
+    try {
+
+        const{name, lastname, password, address, email}=req.body.request;
+        let newUser = await UserModel.create(
+            {
+                User_name: name,
+                User_last_name: lastname,
+                User_password: password,
+                User_address: address,
+                User_Email: email
+            }
+            ,{
+            fields: ['User_name','User_last_name','User_password','User_address','User_Email']
+            }
+        );
+        return 1;
+    } catch (error) {
+        return 0;
+    }  
 }
-module.exports=createUser;
+module.exports={
+    createUser:createUser}
+    ;
