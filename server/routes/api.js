74f00fc875308c48c4f38341424c21953ffe5e66
cerciber <ceatorresar@unal.edu.co>
2020-05-02 13:11:16 -0500

    Clear errors in database and server

--- server/routes/api.js
+++ server/routes/api.js
@@ -23,31 +23,65 @@ const Haulage_Driver_VehicleController = require('../Controllers/Haulage_Driver_
 
 //Function used to check the request fields (valid emails, valid field lengths and valid text fields)
 async function check_fields(req){
-  is_valid = true
   data = req.body.request
-  Object.keys(data).forEach(function(key) {
-    var field = data[key]
-    if((key == 'User_name' || key == 'User_last_name' || key == 'Driver_name' || key == 'Driver_last_name')
+  for (const key of Object.keys(data)) {
+    field = data[key]
+    fieldName = ""
+    if((key == 'User_name' || key == 'Driver_name')
         && !validator.isAlpha(validator.blacklist(field, ' '))){
-        logger.info('Check field: Field must contain only letters')
-       is_valid = false
+        logger.info('Check field: Name "' + field + '" invalid')
+        fieldName = "Nombre"
+        return "El Nombre no es válido"
+    }
+    if((key == 'User_last_name' || key == 'Driver_last_name')
+        && !validator.isAlpha(validator.blacklist(field, ' '))){
+        logger.info('Check field: Lastname "' + field + '" invalid')
+        fieldName = "Apellido"
+        return "El Apellido no es válido"
+    }
+    if((key == 'Driver_password' || key == 'User_password')){
+        fieldName = "Contraseña"
+    }
+    if((key == 'Driver_address' || key == 'User_address')){
+        fieldName = "Dirección"
     }
     if((key == 'Identity_card') && !validator.isNumeric(field)){
-       logger.info('Check field: Field must be a valid document')
-       is_valid = false
+       logger.info('Check field: Identity card "' + field + '" invalid')
+       fieldName = "Cédula"
+       return "La Cédula no es válida"
+    }
+    if((key == 'Driver_phone') && !validator.isNumeric(field)){
+      logger.info('Check field: Phone "' + field + '" invalid')
+      fieldName = "Teléfono"
+      return "El Teléfono no es válido"
     }
     if((key == 'User_Email' || key == 'Driver_Email') && !validator.isEmail(field)){
-       logger.info('Check field: Field must be a valid email')
-       is_valid = false
+       logger.info('Check field: E-Mail "' + field + '" invalid')
+       fieldName = "E-Mail"
+       return "El E-Mail no es válido"
+    }
+    if((key == 'Plate')){
+      fieldName = "Placa"
+    }
+    if((key == 'Brand')){
+      fieldName = "Marca"
+    }
+    if((key == 'Model')){
+      fieldName = "Modelo"
+    }
+    if((key == 'Payload_capacity') && !validator.isNumeric(field)){
+      logger.info('Check field: Payload capacity "' + field + '" invalid')
+      fieldName = "La capacidad de carga"
+      return "La capacidad de carga no es válida"
     }
     //length validation
     if(field.length == 0){
       logger.info("Check field: Field can't be empty")
-      is_valid = false
+      return "El campo '" + fieldName + "' no puede estar vacio"
     }
-  })
+  }
   logger.info("Check field: Field is valid")
-  return is_valid;
+  return true;
 }
 
 async function saveImage(baseImage, path, img_name) {
@@ -65,7 +99,7 @@ async function saveImage(baseImage, path, img_name) {
 
           //write the file
           fs.writeFileSync(fullpath, base64Data, 'base64');
-          logger.error("Save image: Image saved succesfully")
+          logger.info("Save image: Image saved succesfully")
           return true
           //return {filename, localPath};
         }
@@ -87,16 +121,15 @@ router.post('/login', function(req, res){
 //Route will be used to handle driver sign up POST requests
 router.post('/driver/signup', async function(req, res){
   const valid_fields = await check_fields(req);
-  if(valid_fields == false){
-     logger.info('Signup driver: Error in the supplied data')
-     return res.status(400).json({error: 'Error en los datos suministrados '})
+  if(valid_fields !== true){
+     return res.status(400).json({error: valid_fields})
   }
   //Save drivers image
   const filePath = path.join(__dirname, "../public/uploads/drivers/");
   const imageSaved = await saveImage(req.body.request.foto_data, filePath, req.body.request.Identity_card)
   if(imageSaved == false){
-      logger.info('Signup driver: Error in the supplied data')
-      return res.status(400).json({error: 'Error en los datos suministrados '})
+      logger.info('Signup driver: Error in save image')
+      return res.status(400).json({error: 'No se puede guardar la imagen seleccionada'})
   }
 
   //Set the path of the saved image on the db field
@@ -110,29 +143,36 @@ router.post('/driver/signup', async function(req, res){
   if(saved.status == 1)
   {
     logger.info("Signup driver: added succesfully");
-    //res.status(201).send("added succesfully");
     return res.status(201).json({status: 1, db_driver_id: saved.id});
   }
   else{
-    logger.error("Signup driver: " + saved.message);
-    return res.status(500).json({error : saved.message});
+    message = saved.message.toString()
+    logger.error("Signup driver: " + message);
+    if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Driver_Driver_Email_key»") {
+      return res.status(400).json({error : "El E-Mail ya existe"});
+    }
+    if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Driver_Driver_phone_key»") {
+      return res.status(400).json({error : "El Teléfono ya existe"});
+    }
+    if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Driver_Identity_card_key»") {
+      return res.status(400).json({error : "La Cédula ya existe"});
+    }
+    return res.status(500).json({error : message});
   }
 
 });
 
-
 router.post('/vehicle/signup', async function(req, res){
   const valid_fields = await check_fields(req);
-  if(valid_fields == false){
-     logger.info('Signup vehicle: Error in the supplied data')
-     return res.status(400).json({error: 'Error en los datos suministrados '})
+  if(valid_fields !== true){
+     return res.status(400).json({error: valid_fields})
   }
   //Save vehicle image
   const filePath = path.join(__dirname, "../public/uploads/vehicles/");
   const imageSaved = await saveImage(req.body.request.foto_data, filePath, req.body.request.Identity_card)
   if(imageSaved == false){
-      logger.info('Signup vehicle: Error in the supplied data')
-      return res.status(400).json({error: 'Error en los datos suministrados '})
+      logger.info('Signup driver: Error in save image')
+      return res.status(400).json({error: 'No se puede guardar la imagen seleccionada'})
   }
 
   //Set the path of the saved image on the db field
@@ -144,8 +184,21 @@ router.post('/vehicle/signup', async function(req, res){
   let saved_vehicle = await VehicleController.createVehicle(req);
   //error saving the vehicle
   if(saved_vehicle.status != 1 && saved_vehicle.message){
-     logger.error("Signup vehicle: " + saved_vehicle.message);
-     return res.status(500).json({error : saved_vehicle.message});
+     message = saved_vehicle.message.toString()
+     logger.error("Signup vehicle: " + message);
+     if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Vehicle_Plate_key3»") {
+       return res.status(400).json({error : "La Placa ya existe"});
+     }
+     if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Vehicle_Plate_key2»") {
+      return res.status(400).json({error : "La Placa ya existe"});
+     }
+     if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Vehicle_Plate_key1»") {
+       return res.status(400).json({error : "La Placa ya existe"});
+     }
+     if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «Vehicle_Plate_key»") {
+       return res.status(400).json({error : "La Placa ya existe"});
+     }
+     return res.status(500).json({error : message});
   }
 
   //Create driver-vehicle on db using the function: createDriver_Vehicle( Id_driver, Id_vehicle, Is_owner )
@@ -166,26 +219,26 @@ router.post('/vehicle/signup', async function(req, res){
 
 });
 
-
-
 //Route will be used to handle client sign up POST requests
 router.post('/client/signup', async function(req,res){
   const valid_fields = await check_fields(req);
-  if(valid_fields == false){
-     logger.info('Signup client: Error in the supplied data')
-     return res.status(400).json({error: 'Error en los datos suministrados'})
+  if(valid_fields !== true){
+     return res.status(400).json({error: valid_fields})
   }
 
   let success = await UserController.createUser(req);
   if(success==1)
   {
     logger.info("Signup client: added succesfully");
-    //res.status(201).send("added succesfully");
     return res.status(201).json({status: 1});
   }
   else{
-    logger.error("Signup client: " + success.message);
-    res.status(500).json({error : success.message});
+    message = success.toString()
+    logger.error("Signup user: " + message);
+    if (message == "SequelizeUniqueConstraintError: llave duplicada viola restricción de unicidad «User_User_Email_key»") {
+      return res.status(400).json({error : "El E-Mail ya existe"});
+    }
+    return res.status(500).json({error : message});
   }
 }); 
 
@@ -213,8 +266,6 @@ router.get('/driver/schedule', function(req, res){
 router.get('/driver/location', function(req, res){
   res.status(200).json({Api: 'Send driver coordinates'})
 });
-
-
 
 //Redirect unhandled requests
 router.all('*', function(req, res) {
