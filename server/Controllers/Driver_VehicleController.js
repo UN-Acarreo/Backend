ce7bd44017495668a0b7928b07ccfa152bb27f91
cerciber <ceatorresar@unal.edu.co>
2020-05-11 19:36:42 -0500

    Vehicle info controller and return in login

--- server/Controllers/Driver_VehicleController.js
+++ server/Controllers/Driver_VehicleController.js
@@ -1,6 +1,8 @@
 
 // Import model
 const Driver_VehicleModel = require('../Models/Driver_Vehicle');
+const DriverModel = require('../Models/Driver');
+const VehicleModel = require('../Models/Vehicle');
 
 // Import logger
 const logger = require('./../utils/logger/logger');
@@ -30,4 +32,40 @@ async function createDriver_Vehicle( Id_driver, Id_vehicle, Is_owner ) {
     }
 
 }
-module.exports = { createDriver_Vehicle: createDriver_Vehicle };
+
+// Get Vehicle by Driver Id
+//status 0 = Driver not found
+//status 1 = Driver found, Driver returned
+//status -1 = error, error message returned
+async function getVehicleByDriverId(id)
+{
+    //query to find Driver by given email (which is unique)
+    try {
+        let drivers = await DriverModel.findAll(
+            { where: { Id_driver: id }, 
+              attributes: [],
+              include: [{ model: VehicleModel, attributes: ['Plate', 'Brand', 'Model', 'Payload_capacity', 'Photo']}]
+            }
+        )
+        
+        //query returns array of Drivers that match were clause
+        if(drivers.length==0)
+        {
+            logger.info("Driver_VehicleController: id doesnt match known Driver with Vehicle")
+            return {status:0, data:"not found"}
+        }
+        else{
+            logger.info("Driver_VehicleController: Driver vehicle found")
+            //Drivers[0] should be the only Driver in array, .dataValues is Json containing atributes
+            return {status: 1,data: drivers[0].dataValues} 
+        }
+              
+    } catch (error) {
+        logger.info("Driver_VehicleController: "+ error)
+        return {status:-1, data:error}
+        
+    }
+
+}
+
+module.exports = { createDriver_Vehicle: createDriver_Vehicle, getVehicleByDriverId: getVehicleByDriverId };
