38c6461ad9e5bffaff1f800388254422165403a8
afmoyar <afmoyar@unal.edu.co>
2020-06-20 10:20:29 -0500

    billHandler

--- server/BusinessLogic/BusinessLogicProducts/BillHandler.js
+++ server/BusinessLogic/BusinessLogicProducts/BillHandler.js
@@ -9,6 +9,15 @@ async function getBillInfo(Id_haulage)
     let bill = await ControllerFactory.getController("Bill").getRegisterBy({Id_haulage:Id_haulage})
     if(bill.status==1)
     {
-        
+        logger.info("BillHandler/getBillInfo:Success")
+        return {status:1, data:bill.data}
+    }else if(bill.status==0)
+    {
+        logger.info("BillHandler/getBillInfo: No bill information")
+        return {status:0, data:"not found"}
+    }else
+    {
+        logger.error("BillHandler/getBillInfo: "+bill.error)
+        return {status:1, error:bill.error}
     }
 }
